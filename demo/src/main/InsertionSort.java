72a9362684d2aeb15d0a792fde7d7623b270617c
kundyzs <serzhankyzykundyz@gmail.com>
2023-10-25 20:29:10 -0700

    Added InsertionSort.java file

---
+++ demo/src/main/InsertionSort.java
@@ -0,0 +1,19 @@
+package com.sort;
+
+import org.json.simple.*;
+
+public class InsertionSort {
+    private static int count=0;
+
+    public static void main(String[] args) {
+        String fileName =
+          "/Users/jerom/Documents/GitHub/class-java/sort/demo/src/main/java/com/sort/cupcake_3906.json";
+
+          JSONArray cupcakeArray = JSONFile.readArray(fileName);
+          String [] cupcakeNameArray = nameArray(cupcakeArray);
+          System.out.println(cupcakeNameArray);
+          
+
+
+
+}
